81df943d6e0ec42969112d175334244abe4cb2bb
Bhargav Kaklotar <191856996+bhargavkaklotar5@users.noreply.github.com>
2026-05-28 15:46:40 +0530

    Add Flutter app scaffold and CI pipeline

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(appBar: AppBar(title: const Text('CI/CD Pipeline Demo'))),
+  );
+}
